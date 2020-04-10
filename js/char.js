99ab45c6a74a7a9f38323543c1c0126e93aa7b96
Don <donrwalsh@gmail.com>
2020-04-10 08:21:15 -0500

    Refactor char using classes
    Makes for less copypasta implementation.

--- js/char.js
+++ js/char.js
@@ -2,9 +2,139 @@
 
     var char = Char = function() {
 
-        var getLevelUpCost = function() {
-            return this.source.level + 3;
+        class Character {
+            constructor(source, statTier, selector) {
+                this.source = source;
+                this.statTier = statTier;
+                this.selector = selector;
+            }
+
+            getLevelUpCost = function() {
+                return this.source.level + 3;
+            }
+
+            handleLevelUpButton = function() {
+                if (data.experience_points >= this.getLevelUpCost()) {
+                    $(this.selector + 'level-up').addClass('yes').removeClass('no');
+                } else {
+                    $(this.selector + 'level-up').addClass('no').removeClass('yes');
+                }
+            }
+
+            levelUp = function() {
+                if (data.experience_points >= this.getLevelUpCost()) {
+                    updateExp(-1 * this.getLevelUpCost());
+                    this.updateLevel(1);
+                    this.updateHealth(getLevelUpStatIncrease(this.statTier.health, this.source.level))
+                    this.updateStrength(getLevelUpStatIncrease(this.statTier.strength, this.source.level))
+                    this.updateMagic(getLevelUpStatIncrease(this.statTier.magic, this.source.level))
+                    this.updateMind(getLevelUpStatIncrease(this.statTier.mind, this.source.level))
+                    this.updateSpeed(getLevelUpStatIncrease(this.statTier.speed, this.source.level))
+                    this.updateLuck(getLevelUpStatIncrease(this.statTier.luck, this.source.level))
+                }
+            }
+
+            setHealth = function(health) {
+                var original = this.source.health;
+                this.source.health = health;
+                display.update(this.selector + 'health-value', original, health);
+            }
+
+            setLevel = function(level) {
+                var original = this.source.level;
+                this.source.level = level;
+                handleAllLevelUpButtons();
+                display.update(this.selector + 'level-value', original, level);
+                display.update(this.selector + 'level-up-cost', 0, this.getLevelUpCost());
+            }
+
+            setLuck = function(luck) {
+                var original = this.source.luck;
+                this.source.luck = luck;
+                display.update(this.selector + 'luck-value', original, luck);
+            }
+    
+            setMagic = function(magic) {
+                var original = this.source.magic;
+                this.source.magic = magic;
+                display.update(this.selector + 'magic-value', original, magic);
+            }
+    
+            setMind = function(mind) {
+                var original = this.source.mind;
+                this.source.mind = mind;
+                display.update(this.selector + 'mind-value', original, mind);
+            }
+    
+            setSpeed = function(speed) {
+                var original = this.source.speed;
+                this.source.speed = speed;
+                display.update(this.selector + 'speed-value', original, speed);
+            }
+    
+            setStrength = function(strength) {
+                var original = this.source.strength
+                this.source.strength = strength;
+                display.update(this.selector + 'strength-value', original, strength);
+            }
+
+            updateHealth = function(changeBy) {
+                var value = this.source.health + changeBy;
+                this.setHealth(value);
+            }
+    
+            updateLevel = function(changeBy) {
+                var value = this.source.level + changeBy;
+                this.setLevel(value);
+            }
+    
+            updateLuck = function(changeBy) {
+                var value = this.source.luck + changeBy;
+                this.setLuck(value);
+            }
+    
+            updateMagic = function(changeBy) {
+                var value = this.source.magic + changeBy;
+                this.setMagic(value);
+            }
+    
+            updateMind = function(changeBy) {
+                var value = this.source.mind + changeBy;
+                this.setMind(value);
+            }
+    
+            updateSpeed = function(changeBy) {
+                var value = this.source.speed + changeBy;
+                this.setSpeed(value);
+            }
+    
+            updateStrength = function(changeBy) {
+                var value = this.source.strength + changeBy;
+                this.setStrength(value);
+            }
         }
+
+        var warriorStatTier = {
+            strength : 'high', health: 'high',
+            magic : 'low', mind : 'mid',
+            speed : 'low', luck : 'mid'
+        }
+
+        var casterStatTier = {
+            strength : 'low', health: 'mid',
+            magic : 'high', mind : 'high',
+            speed : 'mid', luck : 'low'
+        }
+
+        var rogueStatTier = {
+            strength : 'mid', health: 'low',
+            magic : 'mid', mind : 'low',
+            speed : 'high', luck : 'high'
+        }
+
+        var warrior = new Character(data.warrior, warriorStatTier, '.warrior-');
+        var caster = new Character(data.caster, casterStatTier, '.caster-');
+        var rogue = new Character(data.rogue, rogueStatTier, '.rogue-');
 
         var getLevelUpStatIncrease = function(tier, level) {
             var result = 0;
@@ -26,27 +156,6 @@
             rogue.handleLevelUpButton();
         }
 
-        var handleLevelUpButton = function() {
-            if (data.experience_points >= this.getLevelUpCost()) {
-                $(this.selector + 'level-up').addClass('yes').removeClass('no');
-            } else {
-                $(this.selector + 'level-up').addClass('no').removeClass('yes');
-            }
-        }
-
-        var levelUp = function() {
-            if (data.experience_points >= this.getLevelUpCost()) {
-                updateExp(-1 * this.getLevelUpCost());
-                this.updateLevel(1);
-                this.updateHealth(getLevelUpStatIncrease(this.statTier.health, this.source.level))
-                this.updateStrength(getLevelUpStatIncrease(this.statTier.strength, this.source.level))
-                this.updateMagic(getLevelUpStatIncrease(this.statTier.magic, this.source.level))
-                this.updateMind(getLevelUpStatIncrease(this.statTier.mind, this.source.level))
-                this.updateSpeed(getLevelUpStatIncrease(this.statTier.speed, this.source.level))
-                this.updateLuck(getLevelUpStatIncrease(this.statTier.luck, this.source.level))
-            }
-        }
-
         var setExp = function(amount) {
             var original = data.experience_points;
             data.experience_points = amount;
@@ -54,178 +163,9 @@
             handleAllLevelUpButtons();
         }
 
-        var setHealth = function(health) {
-            var original = this.source.health;
-            this.source.health = health;
-            display.update(this.selector + 'health-value', original, health);
-        }
-
-        var setLevel = function(level) {
-            var original = this.source.level;
-            this.source.level = level;
-            handleAllLevelUpButtons();
-            display.update(this.selector + 'level-value', original, level);
-            display.update(this.selector + 'level-up-cost', 0, this.getLevelUpCost());
-        }
-
-        var setLuck = function(luck) {
-            var original = this.source.luck;
-            this.source.luck = luck;
-            display.update(this.selector + 'luck-value', original, luck);
-        }
-
-        var setMagic = function(magic) {
-            var original = this.source.magic;
-            this.source.magic = magic;
-            display.update(this.selector + 'magic-value', original, magic);
-        }
-
-        var setMind = function(mind) {
-            var original = this.source.mind;
-            this.source.mind = mind;
-            display.update(this.selector + 'mind-value', original, mind);
-        }
-
-        var setSpeed = function(speed) {
-            var original = this.source.speed;
-            this.source.speed = speed;
-            display.update(this.selector + 'speed-value', original, speed);
-        }
-
-        var setStrength = function(strength) {
-            var original = this.source.strength
-            this.source.strength = strength;
-            display.update(this.selector + 'strength-value', original, strength);
-        }
-
         var updateExp = function(changeBy) {
             var value = data.experience_points + changeBy;
             setExp(value);
-        }
-
-        var updateHealth = function(changeBy) {
-            var value = this.source.health + changeBy;
-            this.setHealth(value);
-        }
-
-        var updateLevel = function(changeBy) {
-            var value = this.source.level + changeBy;
-            this.setLevel(value);
-        }
-
-        var updateLuck = function(changeBy) {
-            var value = this.source.luck + changeBy;
-            this.setLuck(value);
-        }
-
-        var updateMagic = function(changeBy) {
-            var value = this.source.magic + changeBy;
-            this.setMagic(value);
-        }
-
-        var updateMind = function(changeBy) {
-            var value = this.source.mind + changeBy;
-            this.setMind(value);
-        }
-
-        var updateSpeed = function(changeBy) {
-            var value = this.source.speed + changeBy;
-            this.setSpeed(value);
-        }
-
-        var updateStrength = function(changeBy) {
-            var value = this.source.strength + changeBy;
-            this.setStrength(value);
-        }
-
-        var warrior = {
-            statTier : {
-                strength : 'high',
-                health: 'high',
-                magic : 'low',
-                mind : 'mid',
-                speed : 'low',
-                luck : 'mid'
-            },
-            source : data.warrior,
-            selector: '.warrior-',
-            getLevelUpCost : getLevelUpCost,
-            handleLevelUpButton : handleLevelUpButton,
-            levelUp : levelUp,
-            setHealth: setHealth,
-            setLevel : setLevel,
-            setLuck : setLuck,
-            setMagic : setMagic,
-            setMind : setMind,
-            setSpeed : setSpeed,
-            setStrength : setStrength,
-            updateHealth: updateHealth,
-            updateLevel : updateLevel,
-            updateLuck : updateLuck,
-            updateMagic : updateMagic,
-            updateMind : updateMind,
-            updateSpeed : updateSpeed,
-            updateStrength : updateStrength
-        }
-
-        var caster = {
-            statTier : {
-                strength : 'low',
-                health: 'mid',
-                magic : 'high',
-                mind : 'high',
-                speed : 'mid',
-                luck : 'low'
-            },
-            source : data.caster,
-            selector: '.caster-',
-            getLevelUpCost : getLevelUpCost,
-            handleLevelUpButton : handleLevelUpButton,
-            levelUp : levelUp,
-            setHealth: setHealth,
-            setLevel : setLevel,
-            setLuck : setLuck,
-            setMagic : setMagic,
-            setMind : setMind,
-            setSpeed : setSpeed,
-            setStrength : setStrength,
-            updateHealth: updateHealth,
-            updateLevel : updateLevel,
-            updateLuck : updateLuck,
-            updateMagic : updateMagic,
-            updateMind : updateMind,
-            updateSpeed : updateSpeed,
-            updateStrength : updateStrength
-        }
-
-        var rogue = {
-            statTier : {
-                strength : 'mid',
-                health: 'low',
-                magic : 'mid',
-                mind : 'low',
-                speed : 'high',
-                luck : 'high'
-            },
-            source : data.rogue,
-            selector: '.rogue-',
-            getLevelUpCost : getLevelUpCost,
-            levelUp : levelUp,
-            handleLevelUpButton : handleLevelUpButton,
-            setHealth: setHealth,
-            setLevel : setLevel,
-            setLuck : setLuck,
-            setMagic : setMagic,
-            setMind : setMind,
-            setSpeed : setSpeed,
-            setStrength : setStrength,
-            updateHealth: updateHealth,
-            updateLevel : updateLevel,
-            updateLuck : updateLuck,
-            updateMagic : updateMagic,
-            updateMind : updateMind,
-            updateSpeed : updateSpeed,
-            updateStrength : updateStrength
         }
     
         return {
