1f7427312af641dffed49645592c82ca6882dd1b
Don <donrwalsh@gmail.com>
2020-12-14 08:55:32 -0600

    Routing controller cleanup
    Removing some no longer used artifacts

--- src/main/java/com/bigbrass/game/rest/controller/RoutingController.java
+++ src/main/java/com/bigbrass/game/rest/controller/RoutingController.java
@@ -1,27 +1,17 @@
 package com.bigbrass.game.rest.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RoutingController {
 
-    // inject via application.properties
-    @Value("${welcome.message:some default}")
-    private String message;
-
-    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
-
     @GetMapping("/")
-    public String main(Model model) {
-        model.addAttribute("message", message);
-        model.addAttribute("tasks", tasks);
-
-        return "home"; //view
+    public ModelAndView main(Model model) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home");
+        return mv;
     }
 }
